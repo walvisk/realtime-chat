72ab4702d92b46506e90218a5fe3675d2b8647d5
walvisk <pujidjayanto@gmail.com>
2023-12-06 05:00:04 +0700

    implement thread to server and client

--- client/client.rb
+++ client/client.rb
@@ -1,8 +1,25 @@
 require "socket"
 
-conn = TCPSocket.new("localhost", 7007)
-conn.puts("Client #{rand(1..10)}")
-loop do
-  mesg = conn.gets
-  puts mesg
+client = TCPSocket.new("localhost", 7007)
+client.puts("Client #{rand(1..10)}")
+
+threads = []
+reading = Thread.new do
+  while incoming = client.gets
+    chat = incoming.chomp
+    puts chat
+  end
 end
+threads << reading
+
+writing = Thread.new do
+  while sent = gets
+    chat = sent.chomp
+    client.puts(chat)
+  end
+end
+threads << writing
+
+threads.each(&:join)
+
+client.close
--- server/server.rb
+++ server/server.rb
@@ -1,14 +1,30 @@
 require "socket"
 
+def handle_client(client, clients)
+  username = client.gets.chomp
+  broadcast_all(clients, client, "#{username} has joined the chat")
+
+  while incoming = client.gets
+    chat = incoming.chomp
+    broadcast_all(clients, client, chat)
+  end
+
+  client.close
+  clients.delete(client)
+  broadcast_all(clients, client, "#{username} has left the chat")
+end
+
+def broadcast_all(clients, client, msg)
+  clients.each { |cl| cl.puts(msg) if cl != client}
+end
 
 server = TCPServer.new("localhost", 7007)
 puts "Server Establihsed at port 7007"
+
+clients = []
 loop do
   client = server.accept
-  if client
-    puts "A Client has been connected"
-  end
-  username = client.gets.chomp
+  clients << client
 
-  client.puts("#{username} has joined the chat")
+  Thread.new{ handle_client(client, clients) }
 end
